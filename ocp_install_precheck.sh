0fceaad4165ed32de8c940944fbc8a2220cde6c2
Bill Strauss <bistraus@redhat.com>
2024-07-15 13:09:41 -0400

    Added GPL v3 License to project

--- ocp_install_precheck.sh
+++ ocp_install_precheck.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# This file is part of the OpenShift Installation Pre-Check Script.
+#
+# The OpenShift Installation Pre-Check Script is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# The OpenShift Installation Pre-Check Script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the OpenShift Installation Pre-Check Script.  If not, see <https://www.gnu.org/licenses/>.
+
 #TODO: add json or other output format later?
 #      fix the broken stuff
 #      work on cleaning up verbose output maybe. probably.
